0a19067c0f8821adc6256b0d8fd614ac4b0d3446
Eng-Tobias <engtobiasguya100@gmail.com>
2024-11-15 17:00:40 -0800

    Fix bubble_sort implementation and add bool handling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,32 @@
 #include "sort.h"
-#include <stdio.h>
+#include <stdbool.h> /* Include this for bool, true, false */
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble sort
- * @array: Pointer to the array to sort
- * @size: Size of the array
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, n;
-    int temp;
-    int swapped;
+	int temp;
+	size_t i, j;
+	bool swapped;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    for (n = size; n > 1; n--)
-    {
-        swapped = 0;
-        for (i = 0; i < n - 1; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                temp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = temp;
-                print_array(array, size);
-                swapped = 1;
-            }
-        }
-        if (!swapped)
-            break;
-    }
+	for (i = 0; i < size - 1; i++) {
+		swapped = false; /* Initialize swapped to false */
+		for (j = 0; j < size - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = true; /* Mark as swapped */
+			}
+		}
+		if (!swapped) /* If no swaps occurred, the array is sorted */
+			break;
+	}
 }
