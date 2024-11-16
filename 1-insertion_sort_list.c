03d0123c9ea54612fdb62552bb23c3eeb747183a
Eng-Tobias <engtobiasguya100@gmail.com>
2024-11-15 18:09:19 -0800

    Implement insertion sort for doubly linked list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,3 +1,7 @@
+#include "sort.h"
+/**
+* swap_nodes - Swaps two nodes in a doubly linked list
+* @list: Pointer to the head of the list
 * @node1: The first node to swap
 * @node2: The second node to swap
 */
@@ -7,27 +11,22 @@ if (node1->prev)
 node1->prev->next = node2;
 else
 *list = node2;
-
 if (node2->next)
 node2->next->prev = node1;
-
 node1->next = node2->next;
 node2->prev = node1->prev;
 node1->prev = node2;
 node2->next = node1;
 }
-
 /**
- * insertion_sort_list - Sorts a doubly linked list using the Insertion sort algorithm
- * @list: Pointer to the head of the list
- */
+* insertion_sort_list - Sorts a doubly linked list using the Insertion sort algorithm
+* @list: Pointer to the head of the list
+*/
 void insertion_sort_list(listint_t **list)
 {
 listint_t *current, *temp;
-
 if (!list || !*list || !(*list)->next)
 return;
-
 current = (*list)->next;
 while (current)
 {
--- sort.h
+++ sort.h
@@ -6,18 +6,18 @@
 /* Structure for doubly linked list */
 typedef struct listint_s
 {
-    const int n;
+    int n;  /* Remove const, as n needs to be modifiable */
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
 
 /* Prototypes */
+listint_t *create_listint(const int *array, size_t size);  /* Add the declaration */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif /* SORT_H */
